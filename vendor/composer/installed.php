9c574bd90b26d37aa4ec82d8c41e05b03d11819e
Augustin Petre <apetre.pro@gmail.com>
2021-03-15 11:29:36 +0100

    fixed carbon fields and added it to future-imperfect theme

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd2ab4c01f673eb9e6cf6eefc9b12180f6b5c8e1a',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd2ab4c01f673eb9e6cf6eefc9b12180f6b5c8e1a',
+    ),
+    'htmlburger/carbon-fields' => 
+    array (
+      'pretty_version' => 'v3.2.3',
+      'version' => '3.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ed32be5868fa500141116afdda520f36f8f735b4',
+    ),
+  ),
+);
